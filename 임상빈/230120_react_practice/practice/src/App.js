b949e4c63f4f85cbfdcf07413121d9b27c57733e
unknown <isangbin@naver.com>
2023-01-20 15:41:58 +0900

    230120 임상빈 commit

---
+++ 임상빈/230120_react_practice/practice/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ColorSchemesExample from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <ColorSchemesExample />
+      </div>
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
