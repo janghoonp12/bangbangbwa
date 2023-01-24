b988030c5b157f2147ba0c067b79c1def55efcf7
정진수 <zxcv423@naver.com>
2023-01-24 18:15:02 +0900

    230124 Broadcast commit
    Former-commit-id: c14fb8c13f0e7718b82e88b4a7c91c58a3bb9e72

--- backend/bangbang/src/main/java/com/bangbang/service/BroadcastService.java
+++ backend/bangbang/src/main/java/com/bangbang/service/BroadcastService.java
@@ -1,15 +1,25 @@
 package com.bangbang.service;
 
 import com.bangbang.vo.Broadcast;
-import java.util.ArrayList;
+import com.bangbang.vo.BroadcastRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 public interface BroadcastService {
 
   String newBroadcast(Broadcast broadCast);         //방송등록
-  ArrayList<Broadcast> searchBroadcastAll();        //방송전체검색
-  ArrayList<Broadcast> searchBroadcastFilter();     //방송필터검색
 
-  String deactivateBroadcast(int broadcast_id);     //방송삭제(비활성화)
 
-  String modifyBroadcast(int broadcast_id);         //방송내용수정
+  List<Broadcast> searchBroadcastAll();             //방송전체검색
+
+
+  List<Broadcast> searchBroadcastFilter();          //방송필터검색
+
+  Broadcast deactivateBroadcast(int broadcastid, Broadcast broadcast);    //방송삭제(비활성화)
+
+
+  Broadcast modifyBroadcast(int broadcastid, Broadcast broadcast);        //방송내용수정
+
 }
