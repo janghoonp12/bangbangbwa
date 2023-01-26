c83a0b42c39437b862996266ab5ef9eea58666a9
강태선 <ts7681@naver.com>
2023-01-26 17:51:29 +0900

    (Feat)Signin
    Former-commit-id: 717e6025695939252ccbae359a7cf514025113a2

--- backend/bangbang/src/main/java/com/bangbang/controller/UserRestController.java
+++ backend/bangbang/src/main/java/com/bangbang/controller/UserRestController.java
@@ -1,17 +1,27 @@
 package com.bangbang.controller;
 
+
+import com.bangbang.service.UserServiceImpl;
+import com.bangbang.vo.SignIn;
 import com.bangbang.vo.User;
-import com.bangbang.vo.UserRepository;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRestController {
 
   @Autowired
-  private UserRepository userRepository;
-
-  @Autowired
-  private BCryptPasswordEncoder bCryptPasswordEncoder;
+  private UserServiceImpl userService;
 
   @GetMapping({"", "/"})
   public String index() {
@@ -30,17 +37,54 @@ public class UserRestController {
   }
 
   @ApiOperation(value = "회원 등록", notes = "회원을 등록합니다.")
-  @PostMapping("/joinForm")
-  @ResponseBody
-  public String join(User user) {
-    user.setUser_role("user");
-    user.setUser_status("1");
-    String rawPassword = user.getUser_password();
-    String encPassword = bCryptPasswordEncoder.encode(rawPassword);
-    user.setUser_password(encPassword);
-    userRepository.save(user);
-    return "redirect:/loginForm";
+  @PostMapping("/signup")
+  @RequestBody
+  public ResponseEntity<?> signUp(User user) throws Exception {
+
+    userService.signUp(user);
+
+    return new ResponseEntity<Object>(new HashMap<String, Object>() {{
+      put("result", true);
+      put("msg", "회원가입을 성공하였습니다.");
+    }}, HttpStatus.OK);
+
   }
 
+  @ApiOperation(value="로그인", notes = "req_data : [id, pw]")
+  @PostMapping("/login")
+  @RequestBody
+  public ResponseEntity<?> login(SignIn user) throws Exception {
+    Map<String, Object> token = userService.login(user);
+    return new ResponseEntity<Object>(new HashMap<String, Object>() {{
+      put("result", true);
+      put("msg", "로그인을 성공하였습니다.");
+      put("access-token", token.get("access-token"));
+      put("refresh-token", token.get("refresh-token"));
+      put("nickname", token.get("nickname"));
+      put("email", token.get("email"));
+      put("id", token.get("id"));
+
+    }}, HttpStatus.OK);
+  }
+
+  @ApiOperation(value = "Access Token 재발급", notes = "만료된 access token을 재발급받는다.")
+  @PostMapping("/refresh")
+  @RequestBody
+  public ResponseEntity<?> refreshToken(Long uid, HttpServletRequest request) throws Exception {
+    HttpStatus status = HttpStatus.ACCEPTED;
+    String token = request.getHeader("refresh-token");
+    String result = userService.refreshToken(uid, token);
+    if (result != null && !result.equals("")) {
+      // 발급 성공
+      return new ResponseEntity<Object>(new HashMap<String, Object>() {{
+        put("result", true);
+        put("msg", "토큰이 발급되었습니다.");
+        put("access-token", result);
+      }}, status);
+    } else {
+      // 발급 실패
+      throw new RuntimeException("리프레시 토큰 발급에 실패하였습니다.");
+    }
+  }
 
 }
--- backend/bangbang/src/main/java/com/bangbang/vo/UserRepository.java
+++ backend/bangbang/src/main/java/com/bangbang/vo/UserRepository.java
@@ -2,7 +2,11 @@ package com.bangbang.vo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    Optional<User> findByUserEmail(String email);
+    Optional<User> findByUserNickname(String nickname);
+    Optional<User> findByUserId(Long uid);
 }
