1f344fd004d285b626300803bc63cdf54a6d60f2
zxzx9404 <zxzx9404@gmail.com>
2023-01-26 14:30:04 +0900

    FE/관심지역 페이지
    Former-commit-id: 298968c0bf61b8f31dd8ef6b81c63859559bcc50

--- frontend/front/src/component/interest/InterestArea.jsx
+++ frontend/front/src/component/interest/InterestArea.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import areaData from "../../interestArea.json";
+import InterestAreaList from "./InterestAreaList";
+
 
 
 function InterestArea() {
-  
+
   return (
     <div>
       <h3 align="center">관심 지역</h3>
+      {areaData.map((area, index) => {
+        return (
+          <InterestAreaList
+              key={`${area.id}`}
+              area={area}
+            />
+        )
+      })}
     </div>
-  )
+  );
 }
 
 export default InterestArea;
