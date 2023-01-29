7842561db0ecd5259d4e6ad88764769c4a80020d
Eunnyeong <doseee114@gmail.com>
2023-01-30 03:36:09 +0900

    feat(item) save관련 repository setting
    Former-commit-id: 0d535320e0cfc189bb3dd81130a4d05357eb32f5

--- backend/bangbang/src/main/java/com/bangbang/service/ItemService.java
+++ backend/bangbang/src/main/java/com/bangbang/service/ItemService.java
@@ -2,14 +2,21 @@ package com.bangbang.service;
 
 import com.bangbang.domain.item.Item;
 
+import com.bangbang.dto.item.ItemPriceSaveRequestDto;
+import com.bangbang.dto.item.ItemSaveRequestDto;
+import com.bangbang.dto.item.ManageOptionSaveRequestDto;
+import com.bangbang.dto.item.OptionSaveRequestDto;
 import java.util.List;
 
 public interface ItemService {
-    void newItem(Item item);
+    long newItem(ItemSaveRequestDto item);
+    void newOption(OptionSaveRequestDto option, long item_id);
+    void newManageOption(ManageOptionSaveRequestDto manageOption, long item_id);
+    void newItemPrice(ItemPriceSaveRequestDto itemPrice, long item_id);
     List<Item> searchItemAll();
     List<Item> searchItemFilter();
-    Item itemDetail(int itemId);
-    void deactivateItem(int itemId);
+    Item itemDetail(long itemId);
+    void deactivateItem(long itemId);
     void modifyItem(Item item);
-    void itemSold(int itemId);
+    void itemSold(long itemId);
 }
