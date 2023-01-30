7fcb8ae531997b4d64d84a24cd0eb46a8f81c7ee
zxzx9404 <zxzx9404@gmail.com>
2023-01-30 15:12:18 +0900

    FE/feat/alarm page
    Former-commit-id: 0fcde08fb06a1667e4617d6bf7510a0a39eb8a84

---
+++ frontend/front/src/component/alarm/AlarmList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import alarmData from "../../alarmData.json";
+import AlarmListItem from "./AlarmListItem";
+
+
+function AlarmList() {
+  return (
+    <div>
+      {alarmData.map((alarm, index) => {
+          return (
+              <AlarmListItem
+                  key={alarm.id}
+                  alarm={alarm}
+              />
+          );
+      })}
+    </div>
+  )
+}
+
+export default AlarmList;
