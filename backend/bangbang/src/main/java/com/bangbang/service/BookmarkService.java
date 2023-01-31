2689dcc887e531677566aeab62e037e1669d77ea
최은녕 <djflsth@naver.com>
2023-01-31 17:42:27 +0900

    fix(bookmark) 동코드 관련 수정
    Former-commit-id: 73c2b7908f49088c2fc2d2ad1d185e8ff0770996

--- backend/bangbang/src/main/java/com/bangbang/service/BookmarkService.java
+++ backend/bangbang/src/main/java/com/bangbang/service/BookmarkService.java
@@ -1,29 +1,31 @@
 package com.bangbang.service;
 
 import com.bangbang.domain.bookmark.BookmarkRepository;
-import com.bangbang.domain.dongcode.Dongcode;
-import com.bangbang.domain.dongcode.DongcodeRepository;
+import com.bangbang.domain.item.DongCodeRepository;
 import com.bangbang.domain.sign.User;
 import com.bangbang.domain.sign.UserRepository;
 import com.bangbang.dto.bookmark.BookmarkSaveRequestDto;
+import com.bangbang.dto.item.SiGuDongDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class BookmarkService {
   private final UserRepository userRepository;
-  private final DongcodeRepository dongcodeRepository;
+  private final DongCodeRepository dongCodeRepository;
   private final BookmarkRepository bookmarkRepository;
 
   //즐겨찾기 등록
   public void newBookmark(BookmarkSaveRequestDto requestDto){
-    Dongcode dongcode = dongcodeRepository.findByDongcodeId(requestDto.getDongcodeId())
-        .orElseThrow(() -> new IllegalArgumentException("해당 동코드가 없습니다."));
+   Optional<SiGuDongDto> dongcode = Optional.of(dongCodeRepository.getAddressName(requestDto.getDongCode()));
+   dongcode.orElseThrow(() -> new IllegalArgumentException("해당 동코드가 없습니다."));
 
     User user = userRepository.findByUserId(requestDto.getUserId())
         .orElseThrow(() -> new IllegalArgumentException("유저 정보가 존재하지 않습니다."));
 
-    bookmarkRepository.save(requestDto.toEntity(user, dongcode));
+    bookmarkRepository.save(requestDto.toEntity());
   }
 }
