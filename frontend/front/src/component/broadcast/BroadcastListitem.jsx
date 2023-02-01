3e2c03b91499177c442202523fe37f7eef3069f6
unknown <isangbin@naver.com>
2023-02-01 15:50:35 +0900

    230201 FE/scroll_error_solved
    Former-commit-id: f6a27be05483d9048ee68ac4c69f2c331e8a4f9d

--- frontend/front/src/component/broadcast/BroadcastListitem.jsx
+++ frontend/front/src/component/broadcast/BroadcastListitem.jsx
@@ -1,21 +1,56 @@
 import React from "react";
-import Card from 'react-bootstrap/Card'; 
+// import Card from 'react-bootstrap/Card';
+import styled from "styled-components";
 
+const SCardDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  height: 400px;
+  margin-right: 50px;
+  margin-bottom: 0px;
+  border: 1px solid grey;
+  border-radius: 8px;
+  cursor: pointer;
+  background: white;
+  :hover {
+    background: lightgrey;
+  }
+  text-align: center;
+`;
+
+const SCardImg = styled.img`
+  width: 250px;
+  height: 250px;
+`;
+
+const SCardBodyDiv = styled.div`
+  // width: 100%;
+`;
+
+const SCardTitleP = styled.p`
+  margin-top: 10px;
+  font-size: 30px;
+`;
+
+const SCardContentP = styled.p`
+  font-size: 20px;
+`;
 
 function BroadcastListItem(props) {
     const { post, onClick } = props;
     return (
-      <Card style={{ width: '15rem', marginLeft: '50px', cursor: 'pointer' }} onClick={onClick}>
-        <Card.Img variant="top" src="logo192.png" alt="이미지" />
-        <Card.Body>
-          <Card.Title>{post.title}</Card.Title>
-          <Card.Text>
+      <SCardDiv onDoubleClick={onClick}>
+        <SCardImg variant="top" src="logo512.png" alt="이미지" />
+        <SCardBodyDiv>
+          <SCardTitleP>{post.title}</SCardTitleP>
+          <SCardContentP>
             {post.type},
             {post.building_type},
             {post.manage_fee}
-          </Card.Text>
-        </Card.Body>
-      </Card>
+          </SCardContentP>
+        </SCardBodyDiv>
+      </SCardDiv>
     )
 }
 
