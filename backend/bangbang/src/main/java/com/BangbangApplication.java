6a133a8d4f74f6a6321d52546d01e378e60b5567
Taesun0727 <77261999+Taesun0727@users.noreply.github.com>
2023-02-01 11:50:23 +0900

    Feat(swagger) redirect 추가
    Former-commit-id: 4bf43cb132c8fca39227057dd6ec57d0dd984570

--- backend/bangbang/src/main/java/com/BangbangApplication.java
+++ backend/bangbang/src/main/java/com/BangbangApplication.java
@@ -1,8 +1,15 @@
 package com;
 
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+import java.net.URI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +17,12 @@ public class BangbangApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BangbangApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return route(GET("/swagger"), req ->
+				ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build());
 	}
 
 }
