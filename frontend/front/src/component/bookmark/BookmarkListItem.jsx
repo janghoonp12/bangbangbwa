df0fb5f4cb53fc7ec11ecfd82c130d5e5b5aff76
zxzx9404 <zxzx9404@gmail.com>
2023-02-01 15:11:45 +0900

    FE/CSS/Bookmark basic CSS
    Former-commit-id: 402ad7cc5a39ccad70d00aef281aa0824aef8ba1

--- frontend/front/src/component/bookmark/BookmarkListItem.jsx
+++ frontend/front/src/component/bookmark/BookmarkListItem.jsx
@@ -3,24 +3,43 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 const SDiv = styled.div`
-  border: 1px solid black;
+  border: 1px solid gold;
   width: 350x;
-  height: 300px;
+  height: 350px;
   margin-right: 20px;
-  cursor: pointer;
+  border-radius: 5px;
 `;
 
 const SImg = styled.img`
   object-fit: cover;
   width: 290px;
   height: 200px;
+  cursor: pointer;
 `;
 
 const SButton = styled.button`
   margin: auto;
   display: block;
-  margin-bottom: 5px;
+  float: right;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  width: 60px;
+  height: 30px;
+  border-radius: 5px;
+  background-color: white;
+  color: black;
+  border: 0.5px solid lightgrey;
+
+  :hover {
+    border: 1px solid black;
+  }
 `;
+
+const SP = styled.p`
+  margin-top: 10px;
+  text-align: center;
+`;
+
 
 function BookmarkListItem(props) {
   const bookmark = props.post;
@@ -28,13 +47,26 @@ function BookmarkListItem(props) {
   const onClick = () => {
     navigate('/')
   }
+  const deleteBookmaark = () => {
+    if (window.confirm(`${bookmark.title}을 삭제하시겠습니까?`)) {
+      alert("삭제되었습니다.");
+    }
+  };
 
   return (
-    <SDiv onClick={onClick}>
-      <h3 align="center"><b>{bookmark.title}</b></h3>
-      <SButton>삭제</SButton>
-      <SImg src={bookmark.img_url} alt="즐겨찾기 이미지" />
-      <p align="center">{bookmark.description}</p>
+    <SDiv>
+      <div>
+        <h3 align="center"><b>{bookmark.title}</b></h3>
+      </div>
+      <div>
+        <SButton onClick={deleteBookmaark}>삭제</SButton>
+      </div>
+      <div>
+        <SImg onClick={onClick} src={bookmark.img_url} alt="즐겨찾기 이미지" />
+      </div>
+      <div>
+        <SP>{bookmark.description}</SP>
+      </div>
     </SDiv>
   )
 }
