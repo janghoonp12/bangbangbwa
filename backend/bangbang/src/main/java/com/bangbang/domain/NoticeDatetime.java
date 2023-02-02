0c372579381d2c10539652ee66eb108f73573e87
최은녕 <djflsth@naver.com>
2023-02-02 12:21:39 +0900

    fix(notice) notice datetime 수정
    Former-commit-id: 42cebd22f103bb820155b59f4bf5eb838a4c94f8

---
+++ backend/bangbang/src/main/java/com/bangbang/domain/NoticeDatetime.java
@@ -0,0 +1,15 @@
+package com.bangbang.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class NoticeDatetime {
+    @CreatedDate
+    private LocalDateTime noticeRegidate;
+}
