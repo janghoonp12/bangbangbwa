44a85bb5af7b6e0aff4abd67d3317d24bf1c05d6
강태선 <ts7681@naver.com>
2023-02-07 00:05:49 +0900

    fix(sign) user Entity, service 변경
    Former-commit-id: a1bd2531613097413a128995aad69baf56cf0cf5

--- backend/bangbang/src/main/java/com/bangbang/domain/sign/User.java
+++ backend/bangbang/src/main/java/com/bangbang/domain/sign/User.java
@@ -1,15 +1,26 @@
 package com.bangbang.domain.sign;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import lombok.Data;
+import javax.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
 @Entity
 @Data
-public class User {
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "user_id")
@@ -26,11 +37,49 @@ public class User {
 
   @Column(length = 100, nullable = true)
   private String user_refresh_token; // 토큰
-
-  @Column(length = 10, nullable = false)
-  private String user_role;             // 유저 등급
+   // 유저 등급
 
   @Column(length = 1, nullable = true)
   private String user_status;             // 유저 상태
 
+  @ElementCollection(fetch = FetchType.EAGER)
+  @Builder.Default
+  private List<String> user_roles = new ArrayList<>();
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return this.user_roles.stream()
+            .map(SimpleGrantedAuthority::new)
+            .collect(Collectors.toList());
+  }
+
+  @Override
+  public String getPassword() {
+    return this.userPassword;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.userNickname;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
