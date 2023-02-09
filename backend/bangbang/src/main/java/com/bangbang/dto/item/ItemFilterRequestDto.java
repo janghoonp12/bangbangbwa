0121c13a1f595945ddb235a37d4a715069f856c5
최은녕 <djflsth@naver.com>
2023-02-09 17:54:04 +0900

    fix(item) 필터 수정중
    Former-commit-id: 6c6247615a59a91f55710232ca9eb81ac60d6f84

--- backend/bangbang/src/main/java/com/bangbang/dto/item/ItemFilterRequestDto.java
+++ backend/bangbang/src/main/java/com/bangbang/dto/item/ItemFilterRequestDto.java
@@ -11,17 +11,19 @@ public class ItemFilterRequestDto {
     private int[] item_deal_type;
     private int[] item_price_buy_house;
     private int[] item_price_house_deposit;
+    private int[] item_price_month_deposit;
     private int[] item_price_month_rent;
     private int[] item_exclusive_area;
     private int[] item_floor;
     private int[] item_build_year;
     private Option option;
 
-    public ItemFilterRequestDto(int[] item_type, int[] item_deal_type, int[] item_price_buy_house, int[] item_price_house_deposit, int[] item_price_month_rent, int[] item_floor, int[] item_build_year, Option option) {
+    public ItemFilterRequestDto(int[] item_type, int[] item_deal_type, int[] item_price_buy_house, int[] item_price_house_deposit, int[] item_price_month_deposit,int[] item_price_month_rent, int[] item_floor, int[] item_build_year, Option option) {
         this.item_type = item_type;
         this.item_deal_type = item_deal_type;
         this.item_price_buy_house = item_price_buy_house;
         this.item_price_house_deposit = item_price_house_deposit;
+        this.item_price_house_deposit = item_price_month_deposit;
         this.item_price_month_rent = item_price_month_rent;
         this.item_floor = item_floor;
         this.item_build_year = item_build_year;
