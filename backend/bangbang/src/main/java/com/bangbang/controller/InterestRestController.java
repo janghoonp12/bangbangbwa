22d492be20155304e24d55b4ed4589f27e1193f1
최은녕 <djflsth@naver.com>
2023-02-10 16:43:44 +0900

    fix(interest) userId 설정 수정
    Former-commit-id: 93811f943997ce80f1a70aeb74b03e62503831f7

--- backend/bangbang/src/main/java/com/bangbang/controller/InterestRestController.java
+++ backend/bangbang/src/main/java/com/bangbang/controller/InterestRestController.java
@@ -8,6 +8,7 @@ import com.bangbang.dto.broker.BrokerSaveRequestDto;
 import com.bangbang.dto.interest.InterestareaSaveRequestDto;
 import com.bangbang.dto.interest.InterestitemSaveRequestDto;
 import com.bangbang.service.InterestService;
+import com.bangbang.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -24,6 +25,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @Api(value="InterestController Version 1")
 @RequiredArgsConstructor
@@ -31,15 +34,19 @@ public class InterestRestController {
     @Autowired
     private final InterestService interestService;
     @Autowired
-    private InterestareaRepository interestareaRepository;
+    private final UserService userService;
 
     @ApiImplicitParams({
         @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
     })
     @ApiOperation(value="관심지역 등록")
     @PostMapping("/user/interest/areas/new")
-    public ResponseEntity<?> newInterestArea(@RequestBody InterestareaSaveRequestDto area) {
+    public ResponseEntity<?> newInterestArea(@RequestBody InterestareaSaveRequestDto area, HttpServletRequest request) {
         try {
+            HttpStatus status = HttpStatus.ACCEPTED;
+            String token = request.getHeader("X-AUTH-TOKEN").substring(7);
+            Long uid = userService.findUserId(token);
+            area.setUserId(uid);
             interestService.newInterestArea(area);
             return new ResponseEntity(HttpStatus.OK);
         } catch (Exception e) {
@@ -53,8 +60,12 @@ public class InterestRestController {
     })
     @ApiOperation(value="관심매물 등록")
     @PostMapping("/user/interest/items/new")
-    public ResponseEntity<?> newInterestItem(@RequestBody InterestitemSaveRequestDto item) {
+    public ResponseEntity<?> newInterestItem(@RequestBody InterestitemSaveRequestDto item, HttpServletRequest request) {
         try {
+            HttpStatus status = HttpStatus.ACCEPTED;
+            String token = request.getHeader("X-AUTH-TOKEN").substring(7);
+            Long uid = userService.findUserId(token);
+            item.setUserId(uid);
             interestService.newInterestItem(item);
             return new ResponseEntity(HttpStatus.OK);
         } catch (Exception e) {
@@ -66,10 +77,13 @@ public class InterestRestController {
         @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
     })
     @ApiOperation(value="관심지역 조회")
-    @GetMapping("/user/interest/areas/{userId}")
-    public ResponseEntity<?> searchInterestArea(@PathVariable Long userId) {
+    @GetMapping("/user/interest/areas")
+    public ResponseEntity<?> searchInterestArea(HttpServletRequest request) {
         try {
-            List<Interestarea> list = interestService.searchInterestArea(userId);
+            HttpStatus status = HttpStatus.ACCEPTED;
+            String token = request.getHeader("X-AUTH-TOKEN").substring(7);
+            Long uid = userService.findUserId(token);
+            List<Interestarea> list = interestService.searchInterestArea(uid);
             if (list != null && !list.isEmpty())
                 return new ResponseEntity<List<Interestarea>>(list, HttpStatus.OK);
             else return new ResponseEntity(HttpStatus.NO_CONTENT);
@@ -82,10 +96,13 @@ public class InterestRestController {
         @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
     })
     @ApiOperation(value="관심매물 조회")
-    @GetMapping("/user/interest/items/{userId}")
-    public ResponseEntity<?> searchInterestItem(@PathVariable Long userId) {
+    @GetMapping("/user/interest/items")
+    public ResponseEntity<?> searchInterestItem(HttpServletRequest request) {
         try {
-            List<Interestitem> list = interestService.searchInterestItem(userId);
+            HttpStatus status = HttpStatus.ACCEPTED;
+            String token = request.getHeader("X-AUTH-TOKEN").substring(7);
+            Long uid = userService.findUserId(token);
+            List<Interestitem> list = interestService.searchInterestItem(uid);
             if (list != null && !list.isEmpty())
                 return new ResponseEntity<List<Interestitem>>(list, HttpStatus.OK);
             else return new ResponseEntity(HttpStatus.NO_CONTENT);
@@ -98,10 +115,10 @@ public class InterestRestController {
         @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
     })
     @ApiOperation(value="관심지역 삭제")
-    @DeleteMapping("/user/interest/areas/{userId}")
-    public ResponseEntity<?> deleteInterestArea(@PathVariable Long userId) {
+    @DeleteMapping("/user/interest/areas/{interestId}")
+    public ResponseEntity<?> deleteInterestArea(@PathVariable Long interestId) {
         try {
-            interestService.deleteInterestArea(userId);
+            interestService.deleteInterestArea(interestId);
             return new ResponseEntity(HttpStatus.OK);
         } catch (Exception e) {
             return exceptionHandling();
@@ -112,10 +129,10 @@ public class InterestRestController {
         @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
     })
     @ApiOperation(value="관심매물 삭제")
-    @DeleteMapping("/user/interest/items/{userId}")
-    public ResponseEntity<?> deleteInterestItem(@PathVariable Long userId) {
+    @DeleteMapping("/user/interest/items/{interestId}")
+    public ResponseEntity<?> deleteInterestItem(@PathVariable Long interestId) {
         try {
-            interestService.deleteInterestItem(userId);
+            interestService.deleteInterestItem(interestId);
             return new ResponseEntity(HttpStatus.OK);
         } catch (Exception e) {
             return exceptionHandling();
