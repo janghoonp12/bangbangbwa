b0c8f4a20dd6cc0920160fd1cc72b4ddecb20ed1
unknown <isangbin@naver.com>
2023-02-11 18:50:30 +0900

    230211 FE/webRTC css
    Former-commit-id: c186199f8f01c849f355ca935e4125033b31ff30

--- frontend/front/src/component/openvidu/UserVideoComponent.js
+++ frontend/front/src/component/openvidu/UserVideoComponent.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import OpenViduVideoComponent from './OvVideo';
 import './UserVideo.css';
 import styled from 'styled-components';
-
+import redDot from '../../assets/reddot.png';
 
 const SP = styled.p`
   margin-bottom: 0px;
+  color: black;
+`;
+
+const SImg = styled.img`
+  width: 10px;
+  height: 10px;
+  padding-bottom: 5%;
 `;
 
 export default class UserVideoComponent extends Component {
@@ -21,7 +28,8 @@ export default class UserVideoComponent extends Component {
                 {this.props.streamManager !== undefined ? (
                     <div className="streamcomponent">
                         <OpenViduVideoComponent streamManager={this.props.streamManager} />
-                        <div><SP>{this.getNicknameTag()}</SP></div>
+                        {/* <div><SP>{this.getNicknameTag()}</SP></div> */}
+                        <div style={{ backgroundColor: "rgba(255, 255, 255, 0.4)"}}><SP><SImg src={redDot} alt="이미지"/> Live</SP></div>
                     </div>
                 ) : null}
             </div>
