3a6df47cd0c2543ef57d38a92500e019498e54c3
zxzx9404 <zxzx9404@gmail.com>
2023-02-13 17:45:34 +0900

    FE/search/broadcast
    Former-commit-id: 9373acf9a28c6af44ffc2064016930f7f9ae777a

--- frontend/front/src/component/item/ItemSearchAll.jsx
+++ frontend/front/src/component/item/ItemSearchAll.jsx
@@ -9,6 +9,24 @@ import SearchItem from "./SearchItem";
 import axios from "axios";
 
 
+const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    margin-top: 30px;
+    align-items: flex-start;
+    justify-content: center;
+    white-space: nowrap;
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+    &::-webkit-scrollbar {
+      display: none;
+    }
+`;
+
 const SButtonDiv = styled.div`
   display: flex;
   justify-content: center;
@@ -29,6 +47,7 @@ function ItemSearchAll() {
 
 
   // 검색 axios 요청
+  const [broadcasts, setBroadcasts] = useState(null);
   const [items, setItems] = useState(null);
   const { searchWord } = useParams();
   const [keyword, dongCode] = searchWord.split('&');
@@ -37,16 +56,25 @@ function ItemSearchAll() {
 
     if (keyword && dongCode) {
       axios.get(`${baseURL}dongCode=${dongCode}&keyword=${keyword}`)
-      .then(res => setItems(res.data.dataB))
+      .then(res => {
+        setItems(res.data.dataB)
+        setBroadcasts(res.data.dataD)
+      })
       .catch(err => console.log(err))
       
     } else if (keyword) {
       axios.get(`${baseURL}keyword=${keyword}`)
-      .then(res => setItems(res.data.dataB))
+      .then(res => {
+        setItems(res.data.dataB)
+        setBroadcasts(res.data.dataD)
+      })
       .catch(err => console.log(err))
     } else if (dongCode) {
       axios.get(`${baseURL}dongCode=${dongCode}`)
-      .then(res => setItems(res.data.dataB))
+      .then(res => {
+        setItems(res.data.dataB)
+        setBroadcasts(res.data.dataD)
+      })
       .catch(err => console.log(err))  
     }
 
@@ -62,7 +90,32 @@ function ItemSearchAll() {
       <div id="filterDiv" style={{ display: "none" }}>
         <Filter />
       </div>
+      <Wrapper>
+        <h2>방송 검색 결과</h2>
+      </Wrapper>
+      <ItemList>
+      {broadcasts && broadcasts.length ? broadcasts.map((broadcast, index) => (
+        <SearchItem
+        broadcasts={broadcasts}
+        />
+      )
+        ) : <h3>검색 결과가 없습니다...</h3>}
+      </ItemList>
       
+      <SButtonDiv>
+        {broadcasts ?
+        <LoadMore 
+          total={broadcasts.length}
+          limit={limit}
+          loads={loads}
+          setLoads={setLoads}
+        /> : null}
+      </SButtonDiv>
+      <hr />
+      <br />
+      <Wrapper>
+        <h2>매물 검색 결과</h2>
+      </Wrapper>
       <ItemList>
       {items && items.length ? items.map((item, index) => (
         <SearchItem
---
+++ frontend/front/src/component/item/SearchBroadcast.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+import { useDispatch } from 'react-redux';
+import { searchDetailItemAsync } from "../../reducers/itemSlice"
+import logosample from "../../assets/logosample.png"
+
+
+const SCardDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  height: 400px;
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-bottom: 0px;
+  border: 1px solid grey;
+  border-radius: 8px;
+  cursor: pointer;
+  background: white;
+  :hover {
+    background: lightgrey;
+  }
+  text-align: center;
+`;
+
+
+const SCardImg = styled.img`
+  width: 248px;
+  height: 250px;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+`;
+
+const SCardBodyDiv = styled.div`
+  // width: 100%;
+`;
+
+const SCardTitleP = styled.p`
+  margin-top: 10px;
+  font-size: 30px;
+`;
+
+const SCardContentP = styled.p`
+  font-size: 20px;
+`;
+
+
+function SearchItem(props) {
+  const broadcast = props.broadcast
+
+  const dispatch = useDispatch();
+  
+  const onClick = () => {
+    dispatch(searchDetailItemAsync(broadcast.item_id))
+  }
+  
+  return (
+      <SCardDiv onDoubleClick={onClick}>
+        <SCardImg variant="top" src={logosample} alt="이미지" />
+        <SCardBodyDiv>
+          <SCardTitleP>{broadcast.broadcast_title}</SCardTitleP>
+          <SCardContentP>
+            {broadcast.broadcast_reservation_time},
+            {broadcast.broadcast_description},
+          </SCardContentP>
+        </SCardBodyDiv>
+      </SCardDiv>
+    )
+}
+
+export default SearchItem;
