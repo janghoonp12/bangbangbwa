5aaa9e2908cc75b31781eafde3d4acd7a17068b2
Taesun0727 <77261999+Taesun0727@users.noreply.github.com>
2023-02-14 09:26:11 +0900

    feat(redux-persist) redux-persist추가
    Former-commit-id: fdb5d0c6c27679c81bd26abbcaa4b46bf9aa3658

--- frontend/front/src/reducers/index.js
+++ frontend/front/src/reducers/index.js
@@ -11,11 +11,23 @@ import bookmarkSlice from "./bookmarkSlice"
 import fileSlice from "./fileSlice";
 import commonSlice from "./commonSlice"
 
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 //axios.defaults.baseURL = "http://localhost:8081/api/"
 axios.defaults.baseURL = "https://i8a405.p.ssafy.io/api"
 
+const persistConfig = {
+    key: "root",
+    // localStorage에 저장합니다.
+    storage,
+    // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
+    whitelist: ["userSlice"]
+    // blacklist -> 그것만 제외합니다
+};
 
-export default combineReducers({
+
+export const rootReducer = combineReducers({
     userSlice,
     noticeSlice,
     itemSlice,
@@ -24,3 +36,5 @@ export default combineReducers({
     fileSlice,
     commonSlice
 })
+
+export default persistReducer(persistConfig, rootReducer);
