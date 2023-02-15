c42cf7f7fc4624b1e65eda60873312fc61a09214
zxzx9404 <zxzx9404@gmail.com>
2023-02-15 14:47:26 +0900

    FE/CSS/Nav 로고 위치 조정, FE/feat/filter 구현
    Former-commit-id: 6f5542557b65b2cc64ecbf2c7a5f81886eacbcdb

---
+++ frontend/front/src/reducers/filterSlice.js
@@ -0,0 +1,94 @@
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const initialState = {
+  filterItemLoading: false,
+  filterItemDone: false,
+  filterItemError: null,
+  filterBroadcastLoading: false,
+  filterBroadcastDone: false,
+  filterBroadcastError: null,
+  filterItem: null, 
+  filterBroadcast: null
+};
+
+export const FilterItemAsync = createAsyncThunk(
+  'item/FILTER_SEARCH',
+  async (data, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        '/items/filter', data
+      );
+      console.log('응답 왔음')
+      return response.data
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err);
+    }
+  }
+);
+
+export const FilterBroadcastAsync = createAsyncThunk(
+  'broadcast/FILTER_SEARCH',
+  async (data, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        '/broadcasts/filter', data
+      );
+      console.log('응답 왔음')
+      return response.data
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err);
+    }
+  }
+);
+
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    initFilterState: (state) => {
+      state.filterItem = null;
+      state.filterBroadcast = null;
+    },
+    clearFilterItemDone: (state) => {
+      state.filterItemDone = false
+    },
+    clearfilterBroadcastDone: (state) => {
+      state.filterBroadcastDone = false
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(FilterItemAsync.pending, (state, action) => {
+      state.filterItemLoading = true;
+      state.filterItemDone = null;
+      state.filterItemError = false;
+    });
+    builder.addCase(FilterItemAsync.fulfilled, (state, action) => {
+      state.filterItemLoading = false;
+      state.filterItemDone = true;
+      state.items = action.payload.content;
+    });
+    builder.addCase(FilterItemAsync.rejected, (state, action) => {
+      state.filterItemLoading = false;
+      state.filterItemError = action.error
+    });
+    builder.addCase(FilterBroadcastAsync.pending, (state, action) => {
+      state.filterBroadcastLoading = true;
+      state.filterBroadcastDone = null;
+      state.filterItemError = false;
+    });
+    builder.addCase(FilterBroadcastAsync.fulfilled, (state, action) => {
+      state.filterBroadcastLoading = false;
+      state.filterBroadcastDone = true;
+      state.items = action.payload.content;
+    });
+    builder.addCase(FilterBroadcastAsync.rejected, (state, action) => {
+      state.filterBroadcastLoading = false;
+      state.filterBroadcastError = action.error
+    });
+  }
+});
+
+export const { initFilterState, clearFilterItemDone, clearfilterBroadcastDone } = filterSlice.actions;
+export default filterSlice.reducer;
