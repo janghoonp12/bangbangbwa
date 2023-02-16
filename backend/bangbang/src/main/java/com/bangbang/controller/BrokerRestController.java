9be88e25a416a1cbbf1bc3d9ebcd3e2affa98f34
최은녕 <djflsth@naver.com>
2023-02-16 12:19:29 +0900

    fix(broker) broker 조회
    Former-commit-id: b86c7b7ca1c8fb78eadc41dfb943ec4f5e9e4679

--- backend/bangbang/src/main/java/com/bangbang/controller/BrokerRestController.java
+++ backend/bangbang/src/main/java/com/bangbang/controller/BrokerRestController.java
@@ -1,5 +1,8 @@
 package com.bangbang.controller;
 
+import com.bangbang.domain.broadcast.BroadcastRepository;
+import com.bangbang.domain.broker.Broker;
+import com.bangbang.domain.broker.BrokerRepository;
 import com.bangbang.dto.broker.BrokerResponseDto;
 import com.bangbang.dto.broker.BrokerSaveRequestDto;
 import com.bangbang.service.BrokerService;
@@ -26,6 +29,10 @@ public class BrokerRestController {
 
     @Autowired
     private final UserService userService;
+    @Autowired
+    private BrokerRepository brokerRepository;
+    @Autowired
+    private BroadcastRepository broadcastRepository;
 
 
     @ApiOperation(value="중개사 신청")
@@ -82,6 +89,23 @@ public class BrokerRestController {
             Long uid = userService.findUserId(token);
             brokerService.deactiveBroker(uid);
             return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            return exceptionHandling();
+        }
+    }
+
+    @ApiOperation(value="중개사 정보 조회")
+    @GetMapping("/broker/brokers")
+    public ResponseEntity<?> searchBroker(HttpServletRequest request) {
+        try {
+            HttpStatus status = HttpStatus.ACCEPTED;
+            String token = request.getHeader("X-AUTH-TOKEN");
+            Long uid = userService.findUserId(token);
+            Long brokerId = brokerRepository.findByUserId(uid).getBrokerId();
+            Broker broker = brokerService.searchBroker(brokerId);
+            if (broker != null)
+                return new ResponseEntity<Broker>(broker, HttpStatus.OK);
+            else return new ResponseEntity(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
             return exceptionHandling();
         }
