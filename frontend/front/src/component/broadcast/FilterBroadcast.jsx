cd86a132df97fe9f43c8c51c7d8645af966aa0b0
zxzx9404 <zxzx9404@gmail.com>
2023-02-16 09:22:31 +0900

    FE/feat/filter수정, filterbroadcast page
    Former-commit-id: 146f60503d554d7dd1802e8fa1c075b57f6e1088

--- frontend/front/src/component/broadcast/FilterBroadcast.jsx
+++ frontend/front/src/component/broadcast/FilterBroadcast.jsx
@@ -1,12 +1,57 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Filter from "../common/Filter";
+import FilterButton from "../common/FilterButton";
+import styled from "styled-components";
+import ItemList from "../item/ItemList";
+import SearchBroadcast from "../item/SearchBroadcast";
 
+const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    margin-top: 30px;
+    align-items: flex-start;
+    justify-content: center;
+    white-space: nowrap;
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+    &::-webkit-scrollbar {
+      display: none;
+    }
+`;
 
+const SButtonLineDiv = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 8fr 2fr;
+`;
 
 function FilterBroadcast() {
-
-  return(
+  const { filterBroadcast } = useSelector((state) => state.filterSlice);
+  
+  return (
     <div>
-      FilterBroadcast Area
+      <SButtonLineDiv>
+        <div />
+        <FilterButton />
+      </SButtonLineDiv>
+      <div id="filterDiv" style={{ display: "none" }}>
+        <Filter />
+      </div>
+      <Wrapper>
+        <h2>방송 검색 결과</h2>
+      </Wrapper>
+      <ItemList>
+      {filterBroadcast && filterBroadcast.length ? filterBroadcast.map((broadcast, index) => (
+        <SearchBroadcast
+        broadcast={broadcast}
+        />
+      )
+        ) : <h3>검색 결과가 없습니다...</h3>}
+      </ItemList>
     </div>
   )
 }
